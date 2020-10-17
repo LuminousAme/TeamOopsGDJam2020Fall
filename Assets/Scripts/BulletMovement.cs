f4beb81e8d50b125dbd3c80929737371479c3b86
Ame <amegilham@gmail.com>
2020-10-16 21:05:25 -0400

    added water shader, basic movement, and shooting

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    //the direction the bullet should be travelling
+    private Vector3 dir;
+    //the speed the bullet should be moving at 
+    public float speed = 10;
+
+    //when the bullet is first fired
+    public void onFire(Vector3 shootDir)
+    {
+        //copy the direction the bullet was fired in 
+        dir = shootDir.normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move the bullet in that direction
+        transform.position += dir * speed * Time.deltaTime;        
+    }
+}
